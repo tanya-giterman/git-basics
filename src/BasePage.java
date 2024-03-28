b471940276b3a65fe338d7cf4cc8b697c76b85cd
tanya-giterman <tanya.giterman@gmail.com>
2024-03-27 20:09:06 -0400

    BasePage file work in progress

---
+++ src/BasePage.java
@@ -0,0 +1,10 @@
+public class BasePage {
+    public static void main(String[] args) {
+        String driver;
+
+
+    }
+    public void navigteToMenuItem(String menuItem){
+        System.out.println("navigate to " + menuItem);
+    }
+}
