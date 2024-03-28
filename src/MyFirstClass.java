aed16335afe3cf552b012784c59abd507ff5fc63
tanya-giterman <tanya.giterman@gmail.com>
2024-03-27 20:58:57 -0400

    Updated

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -2,6 +2,6 @@ public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I just changed this line even more to new line");
-        System.out.println("new line");
+
     }
 }
